922c7275449977477771ffad6dabcf3bfeb7da7c
youngmok9870 <93384025+youngmok9870@users.noreply.github.com>
2021-11-06 19:30:33 +0900

    Add files via upload

---
+++ linkAutoTradeWithMA.py
@@ -0,0 +1,67 @@
+import time
+import pyupbit
+import datetime
+
+access = "OmSI1rhBty9XZDCJpyZew3WBfc6ZVw4N3J7fYTIR"
+secret = "JPrQxxMZXr6exppYqov8iGONWrBr3PWoOkdvWmcf"
+
+def get_target_price(ticker, k):
+    """변동성 돌파 전략으로 매수 목표가 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
+    return target_price
+
+def get_start_time(ticker):
+    """시작 시간 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    start_time = df.index[0]
+    return start_time
+
+def get_ma20(ticker):
+    """15일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=20)
+    ma20 = df['close'].rolling(20).mean().iloc[-1]
+    return ma20
+
+def get_balance(ticker):
+    """잔고 조회"""
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+    return 0
+
+def get_current_price(ticker):
+    """현재가 조회"""
+    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
+
+# 로그인
+upbit = pyupbit.Upbit(access, secret)
+print("autotrade start")
+
+# 자동매매 시작
+while True:
+    try:
+        now = datetime.datetime.now()
+        start_time = get_start_time("KRW-LINK")
+        end_time = start_time + datetime.timedelta(days=1)
+
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
+            target_price = get_target_price("KRW-LINK", 0.5)
+            ma20 = get_ma20("KRW-LINK")
+            current_price = get_current_price("KRW-LINK")
+            if target_price < current_price and ma20 < current_price:
+                krw = get_balance("KRW")
+                if krw > 5000:
+                    upbit.buy_market_order("KRW-LINK", krw*0.9995)
+        else:
+            link = get_balance("LINK")
+            if link > 0.00008:
+                upbit.sell_market_order("KRW-LINK", link*0.9995)
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        time.sleep(1)
